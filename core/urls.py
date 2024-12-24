ac5cffc9c67f5555b1f4d2900f7213f60e9a40a8
Gaurav patel <164714270+Gaurav0135@users.noreply.github.com>
2024-12-24 12:05:54 +0530

    Add files via upload

---
+++ core/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('settings', views.settings, name='settings'),
+    path('upload', views.upload, name='upload'),
+    path('follow', views.follow, name='follow'),
+    path('search', views.search, name='search'),
+    path('profile/<str:pk>', views.profile, name='profile'),
+    path('like-post', views.like_post, name='like-post'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('logout', views.logout, name='logout'),
+    path('Notic', views.Notic, name="Notic"),
+    path('Notice', views.create_notice, name="Notice"),
+    path('notice', views.notice, name="notice"),
+    path('notice1', views.Notic, name='notice1'),  
+]
